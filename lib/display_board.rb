e7d09a281e74731090a2b359512369b70b7fb0a0
John Warts <creepervoncreeps@gmail.com>
2018-06-29 03:54:52 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,13 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-def display_board(board)
-
-puts 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
-
-
-
-
+def display_board
+    puts "   |   |   "
+    puts "-----------"
+    puts "   |   |   "
+    puts "-----------"
+    puts "   |   |   "
 end
+
