bcfa000aa46bf5d76747015264bb2289f2e64154
John Warts <creepervoncreeps@gmail.com>
2018-06-29 03:57:55 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@
 
 def display_board(board)
 
-    board = 
+    board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
 
     #set the board values
 
@@ -14,9 +14,9 @@ def display_board(board)
 
 
 
-    puts "   |   |   "
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
     puts "-----------"
-    puts "   |   |   "
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
     puts "-----------"
-    puts "   |   |   "
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
