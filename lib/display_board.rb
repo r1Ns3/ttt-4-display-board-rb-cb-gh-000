721dd93af58dc74e03ad679231959666fe79d6c3
John Warts <creepervoncreeps@gmail.com>
2018-06-29 03:56:50 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,22 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-def display_board
+def display_board(board)
+
+    board = 
+
+    #set the board values
+
+
+
+
+
+
+
+
     puts "   |   |   "
     puts "-----------"
     puts "   |   |   "
     puts "-----------"
     puts "   |   |   "
 end
-
